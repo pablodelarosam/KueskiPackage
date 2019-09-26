8439a1eecafa65036319691268960df1738aca78
Pablo de la Rosa Michicol <pablo.delarosaml@udlap.com.mx>
2019-09-26 14:29:28 -0500

    Initial Commit

---
+++ Sources/KueskiPackage/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pablo de la Rosa Michicol on 9/26/19.
+//
+
+import Foundation
--- Sources/KueskiPackage/KueskiPackage.swift
+++ Sources/KueskiPackage/KueskiPackage.swift
@@ -1,3 +1,30 @@
+import UIKit
 struct KueskiPackage {
     var text = "Hello, World!"
+}
+
+
+open class KueskiCustomButton: UIView {
+    
+    let kueskiButton: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        button.setTitle("KUESKI", for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+       return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        self.addSubview(kueskiButton)
+        kueskiButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        kueskiButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
